78aa6926304a214daf332ef215f7acd2b1952a72
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 06:12:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,11 +14,14 @@ sender.valid? && receiver.valid?
 end
 
 def execute_transaction
- if self.valid? && self.amount < @sender.balance && @status =="pending"
+ #if self.valid? && self.amount < @sender.balance && @status =="pending"
+
  @sender.balance = @sender.balance - @amount
+
  @receiver.balance = @receiver.balance + @amount 
+ 
  @status == "complete"
- elsif !sender.valid?
+ if !sender.valid?
    @status == "rejected"
    puts "Transaction rejected. Please check your account balance."
   
