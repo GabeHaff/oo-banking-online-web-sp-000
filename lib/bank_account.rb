91d89a50e54577c0082e6f4e7da72f4383ab3ea2
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 05:07:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,10 +5,12 @@ class BankAccount
   #expect: can transfer money to another BankAccount, using Transfer class.
 
 
-def initialize(name, balance=1000, status="open")
+def initialize(name#, balance=1000, status="open")
   @name = name
-  @balance = balance
-  @status = status
+  @balance = 1000
+  @status = "open"
+ # @balance = balance
+ # @status = status
 end 
 #initializes w/name, balance of 1000, status of "open", name cannot be changed. 
 
