4f8c3ebf59522d6d32c7b04ed6fe8c013dda5b2b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 06:27:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,4 @@
 class Transfer
-#  attr_reader :sender, :receiver, :amount
   attr_accessor :sender, :receiver, :amount, :status
   
 def initialize(sender, receiver, amount)
@@ -33,37 +32,3 @@ def execute_transaction
   end 
  
  
- #- can execute a successful transaction between two accounts (FAILED - 1)
-   # each transfer can only happen once (FAILED - 2)
-    #rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 3)
-  #the transaction from sender, add it to receiver account
-  
-  #reverse_transfer
-   # can reverse a transfer between two accounts (FAILED - 4)
-   # it can only reverse executed transfers 
-
-end #class end 
-
-
- #status = [:pending, :complete, :rejected, reversed]// status.pending? status.rejected? etc 
-
-  #valid?
-   # can check that both accounts are valid (FAILED - 10)
-   # calls on the sender and receiver's #valid? methods (FAILED - 11)
-  #execute_transaction
-   # can execute a successful transaction between two accounts (FAILED - 12)
-   # each transfer can only happen once (FAILED - 13)
-   # rejects a transfer if the sender does not have enough funds (does not have a valid account) (FAILED - 14)
-  #reverse_transfer
-   # can reverse a transfer between two accounts (FAILED - 15)
-   # it can only reverse executed transfers (FAILED - 16)
-
-#goals:
-#1:
-#checks validity of bank acc; checks for funds(pending->complete)/insufficient funds(pending->rejected); || status = "complete" -> "reversed"
-
-#2:
-#default "status" = "pending"; status="pending"->status="complete" || status = "rejected";
-#status= "completed" Transfer is able to be "reversed". 
-#status = flag? default value that changes based on input/args?
-
