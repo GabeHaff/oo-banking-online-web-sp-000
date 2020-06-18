1821c1b768e23120c7108c77f63dc3c823f77b2a
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 05:17:44 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,21 +1,13 @@
-require 'pry'
 class BankAccount
   attr_reader :name
   attr_accessor :balance, :status
-  #expect: can transfer money to another BankAccount, using Transfer class.
 
 
 def initialize(name, balance=1000, status="open")
   @name = name
   @balance = balance
   @status = status
-  #@balance = 1000
-  #@status = "open"
-  
 end 
-#initializes w/name, balance of 1000, status of "open", name cannot be changed. 
-
- 
 
 def deposit(deposit_amount)
 @balance+= deposit_amount
@@ -33,9 +25,5 @@ def close_account
   @status = "closed"
 end 
   
-  #close_account
-   # can close its account (FAILED - 4)
-
-
 
 end #class end 
