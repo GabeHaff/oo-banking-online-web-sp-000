5849e4e738cab2e8f0da3a020f9df572204bac3b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 05:11:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -22,7 +22,7 @@ def deposit(deposit_amount)
 end 
 
 def display_balance 
-  puts "Your balance is $ #{@balance}."
+"Your balance is $#{@balance}."
 end 
   #display_balance
     #can display its balance (FAILED - 2)
