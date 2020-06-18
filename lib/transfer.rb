ff04fe416138c7ecb3784ae77fcc732738c28662
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-06-18 05:45:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ sender.valid? && receiver.valid?
 end
 
 def execute_transaction
-  @sender.balance - amount && @receiver.balance + amount 
+  @sender.balance - @amount && @receiver.balance + @amount 
   
   #- the transaction from sender, add it to receiver account
 end 
